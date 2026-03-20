db92dfa21bc3ec59d9c127326fc229f13b250070
Akshay <komaraakshay1@gmail.com>
2026-03-20 08:56:49 +0530

    Solved async-js-easy

--- JavaScript-Assignments/01-async-js/easy/3-read-from-file.js
+++ JavaScript-Assignments/01-async-js/easy/3-read-from-file.js
@@ -5,3 +5,20 @@
 // Try to do an expensive operation below the file read and see how it affects the output. 
 // Make the expensive operation more and more expensive and see how it affects the output. 
 
+const fs = require('fs');
+
+// Async file read
+fs.readFile('file.txt', 'utf8', function(err, data) {
+    if (err) {
+        console.log("Error reading file:", err);
+        return;
+    }
+    console.log("File content:", data);
+});
+
+// Expensive operation below file read
+let sum = 0;
+for (let i = 0; i < 1000000; i++) {
+    sum += i;
+}
+console.log("Expensive operation done! Sum:", sum);
--- JavaScript-Assignments/01-async-js/easy/4-write-to-file.js
+++ JavaScript-Assignments/01-async-js/easy/4-write-to-file.js
@@ -2,3 +2,14 @@
 
 // Using the fs library again, try to write to the contents of a file.
 // You can use the fs library to as a black box, the goal is to understand async tasks.
+
+const fs = require('fs');
+
+// Async file write
+fs.writeFile('file.txt', 'Hello World!', function(err) {
+    if (err) {
+        console.log("Error writing file:", err);
+        return;
+    }
+    console.log("File written successfully!");
+});
